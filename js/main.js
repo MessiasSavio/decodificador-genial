65aad7d9303840fdb0a322b1988c24a307da57d9
messias_savio <saviochavesadm@gmail.com>
2024-02-09 22:41:05 -0300

    js:todas as funções criadas; CSS faltando apenas Responsividade

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@ function descriptografar(texto) {
 
 function validarTexto(texto) {
   // Verifica se o texto contém letras maiúsculas ou caracteres acentuados
-  if (/[A-ZÁÉÍÓÚÀÈÌÒÙÂÊÎÔÛÃÕÄËÏÖÜ]/.test(texto)) {
+  if (/[A-ZÁÉÍÓÚÀÈÌÒÙÂÊÎÔÛÃÕÄËÏÖÜzáéíóúàèìòùâêîôûãõäëïöü]/.test(texto)) {
     alert("Por favor, digite apenas letras minúsculas e sem acento.");
     return false;
   }
@@ -34,15 +34,24 @@ function textoCriptografado() {
   if (!validarTexto(texto)) {
     return;
   }
+
+  // Verifica se o texto contém pelo menos uma vogal
+  if (!/[aeiou]/.test(texto)) {
+    alert('Presisamos de ao menos 1 vogal para ser criptografada');
+    alert('Digite algo para ser criptografado ou descriptografado!');
+    return;
+  }
+
   var textoCriptografado = criptografar(texto);
   document.getElementById("texto_saida").value = textoCriptografado;
-  
+
   if (texto.trim() !== "") {
     esconderImagem();
     mostrarBotao();
   } else {
     mostrarImagem();
-}
+    alert('Digite algo para ser criptografado ou descriptografado!');
+  }
 }
 
 function descriptografarTexto() {
@@ -50,6 +59,15 @@ function descriptografarTexto() {
   if (!validarTexto(texto)) {
     return;
   }
+  
+  // Verifica se o texto contém pelo menos uma ocorrência de uma das palavras-chave
+  if (!texto.includes("enter") && !texto.includes("imes") && !texto.includes("ai") && !texto.includes("ober") && !texto.includes("ufat")) {
+    alert('Não há nada a ser descriptografado.');
+    mostrarImagem();
+    alert('Digite algo para ser criptografado ou descriptografado!');
+    return;
+  }
+
   var textoDescriptografado = descriptografar(texto);
   document.getElementById("texto_saida").value = textoDescriptografado;
 
@@ -58,10 +76,9 @@ function descriptografarTexto() {
     mostrarBotao();
   } else {
     mostrarImagem();
+    alert('Digite algo para ser criptografado ou descriptografado!');
   }
 }
-
-
 
 function esconderImagem() {
   var imagem = document.getElementById("img_inicial");
@@ -74,25 +91,45 @@ function esconderImagem() {
 function mostrarImagem() {
   var imagem = document.getElementById("img_inicial");
   var mensagem = document.getElementById("mensagem");
-  var mensagem = document.getElementById("mensagem");
-
+  let texto = document.getElementById("texto_saida");
   imagem.style.display = "block";
-  imagem.style.alignItems = "center";
-  
-  imagem.style.marginRight ="25%";
-  mensagem.style.display = "block";
-  mensagem.style.textAlign ="center";
-
+  mensagem.style.display = 'block';
+  mensagem.style.textAlign = "center";
+  BotaoCopiar.style.display = "none";
+  texto.style.display = "none";
 }
+const digitar1 = document.querySelector("#texto_inicial")
+const digitar2 = document.querySelector("#texto_saida")
+const btnCopiar = document.getElementById("copiar")
+
 
 function copy() {
-  var copiar = document.getElementById("copiar");
-  navigator.clipboard.writeText(copiar.value);
+  navigator.clipboard.writeText(digitar2.value);
+
+  // Alterar o texto do botão para "Copiado!" imediatamente
+  btnCopiar.innerText = "Copiado!";
+  btnCopiar.style.backgroundColor = "green";
+  btnCopiar.style.color = "white";
+  btnCopiar.style.fontSize = "25px"
+
+
+  // Agendar a mudança de volta para o texto original após 5 segundos
+  setTimeout(function() {
+    btnCopiar.innerText = "Copiar";
+    btnCopiar.style.backgroundColor = "var(--cor--quintenaria)";
+    btnCopiar.style.color = "white";
+  }, 3000);
 }
+
+// Adicionar um ouvinte de evento de clique ao botão
+btnCopiar.addEventListener("click", copy);
+
+
 let BotaoCopiar = document.getElementById("copiar");
 BotaoCopiar.style.display = "none";
 
 function mostrarBotao() {
   BotaoCopiar.style.display = "block";
-
+  let textArea = document.getElementById('texto_saida')
+  textArea.style.display = "flex"
 }
